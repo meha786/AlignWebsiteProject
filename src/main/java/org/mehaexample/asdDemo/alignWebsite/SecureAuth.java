90e95cdc9c3616d11060ce16f95103b14e4a4ce1
Meha Verma <meha786@ccs.neu.edu>
2018-03-27 08:48:03 -0700

    Uncommented Secure Auth

--- src/main/java/org/mehaexample/asdDemo/alignWebsite/SecureAuth.java
+++ src/main/java/org/mehaexample/asdDemo/alignWebsite/SecureAuth.java
@@ -38,52 +38,52 @@ public class SecureAuth implements ContainerRequestFilter{
 			throws IOException {
 		if(!requestContext.getUriInfo().getPath().contains(LOGIN_URL)){
 		List<String> authHeader =  requestContext.getHeaders().get(AUTHOIRIZATION_HEADER);
-//		if(authHeader.size() > 0){
-//			try {
-//			String authToken = authHeader.get(0);			
-//			String ip = sr.getRemoteAddr();
-//			String secretKey = ip+"sEcR3t_nsA-K3y";
-//			byte[] key = secretKey.getBytes();
-//			key = Arrays.copyOf(key, 32);
-//			AesKey keyMain = new AesKey(key);
-//			
-//			JsonWebEncryption receiverJwe = new JsonWebEncryption();
-//			AlgorithmConstraints algConstraints = new AlgorithmConstraints(ConstraintType.WHITELIST,KeyManagementAlgorithmIdentifiers.DIRECT);
-//		    receiverJwe.setAlgorithmConstraints(algConstraints);
-//		    AlgorithmConstraints encConstraints = new AlgorithmConstraints(ConstraintType.WHITELIST, ContentEncryptionAlgorithmIdentifiers.AES_128_CBC_HMAC_SHA_256);
-//		    receiverJwe.setContentEncryptionAlgorithmConstraints(encConstraints);
-//		    receiverJwe.setCompactSerialization(authToken);
-//		    receiverJwe.setKey(keyMain);
-//		    String plaintext;
-//			plaintext = receiverJwe.getPlaintextString();
-//			StringTokenizer tokenData = new StringTokenizer(plaintext,"*#*");
-//			String email = tokenData.nextToken();
-//			String ipAddress = tokenData.nextToken();
-//			String timeValid = tokenData.nextToken();
-//			
-//			AdminLogins adminLogins = adminLoginsDao.findAdminLoginsByEmail(email);
-//			
-//			if(adminLogins == null){
-//				requestContext.abortWith(Response.status(Response.Status.NOT_ACCEPTABLE).
-//						entity("Token not valid. Please login again.").build());
-//			} else if(ip.equals(ipAddress)) {
-//				Timestamp keyExpiration = new Timestamp(System.currentTimeMillis()+15*60*1000);
-//				adminLogins.setKeyExpiration(keyExpiration);
-//				adminLoginsDao.updateAdminLogin(adminLogins);
-//				return;
-//			} else {
-//				requestContext.abortWith(Response.status(Response.Status.NOT_ACCEPTABLE).
-//						entity("Token not valid. Please login again.").build());
-//			}
-//			
-//			} catch (Exception e) {
-//				requestContext.abortWith(Response.status(Response.Status.NOT_ACCEPTABLE).
-//						entity("Token not valid. Please login again.").build());
-//			}
-//		} else {
-//			requestContext.abortWith(Response.status(Response.Status.NOT_ACCEPTABLE).
-//					entity("Token not valid. Please login again.").build());
-//		}
+		if(authHeader.size() > 0){
+			try {
+			String authToken = authHeader.get(0);			
+			String ip = sr.getRemoteAddr();
+			String secretKey = ip+"sEcR3t_nsA-K3y";
+			byte[] key = secretKey.getBytes();
+			key = Arrays.copyOf(key, 32);
+			AesKey keyMain = new AesKey(key);
+			
+			JsonWebEncryption receiverJwe = new JsonWebEncryption();
+			AlgorithmConstraints algConstraints = new AlgorithmConstraints(ConstraintType.WHITELIST,KeyManagementAlgorithmIdentifiers.DIRECT);
+		    receiverJwe.setAlgorithmConstraints(algConstraints);
+		    AlgorithmConstraints encConstraints = new AlgorithmConstraints(ConstraintType.WHITELIST, ContentEncryptionAlgorithmIdentifiers.AES_128_CBC_HMAC_SHA_256);
+		    receiverJwe.setContentEncryptionAlgorithmConstraints(encConstraints);
+		    receiverJwe.setCompactSerialization(authToken);
+		    receiverJwe.setKey(keyMain);
+		    String plaintext;
+			plaintext = receiverJwe.getPlaintextString();
+			StringTokenizer tokenData = new StringTokenizer(plaintext,"*#*");
+			String email = tokenData.nextToken();
+			String ipAddress = tokenData.nextToken();
+			String timeValid = tokenData.nextToken();
+			
+			AdminLogins adminLogins = adminLoginsDao.findAdminLoginsByEmail(email);
+			
+			if(adminLogins == null){
+				requestContext.abortWith(Response.status(Response.Status.NOT_ACCEPTABLE).
+						entity("Token not valid. Please login again.").build());
+			} else if(ip.equals(ipAddress)) {
+				Timestamp keyExpiration = new Timestamp(System.currentTimeMillis()+15*60*1000);
+				adminLogins.setKeyExpiration(keyExpiration);
+				adminLoginsDao.updateAdminLogin(adminLogins);
+				return;
+			} else {
+				requestContext.abortWith(Response.status(Response.Status.NOT_ACCEPTABLE).
+						entity("Token not valid. Please login again.").build());
+			}
+			
+			} catch (Exception e) {
+				requestContext.abortWith(Response.status(Response.Status.NOT_ACCEPTABLE).
+						entity("Token not valid. Please login again.").build());
+			}
+		} else {
+			requestContext.abortWith(Response.status(Response.Status.NOT_ACCEPTABLE).
+					entity("Token not valid. Please login again.").build());
+		}
 	} else {
 		return;
 	}
