b5657118d459ff49ba4551ee08f81a44588d4395
krishnakaranam <karanam.s@husky.neu.edu>
2018-04-02 13:18:00 -0400

    changed cors filter for debug

--- src/main/java/org/mehaexample/asdDemo/alignWebsite/CorsFilter.java
+++ src/main/java/org/mehaexample/asdDemo/alignWebsite/CorsFilter.java
@@ -12,6 +12,6 @@ public class CorsFilter implements ContainerResponseFilter{
 	public void filter(ContainerRequestContext requestContext,   ContainerResponseContext responseContext)
 	    throws IOException {
 	        responseContext.getHeaders().add("Access-Control-Allow-Origin","*");
-	        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
+	        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT, OPTIONS");
 	  }
 }
